208999bb6f75a3400772fb904a771d8465a2ca8c
Caíque <135059792+CaFranca@users.noreply.github.com>
2025-04-02 11:28:03 -0300

    Nomes

--- src/br/edu/ifsp/spo/java/cards/App.java
+++ src/br/edu/ifsp/spo/java/cards/App.java
@@ -1,10 +1,23 @@
 package br.edu.ifsp.spo.java.cards;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String nome1;
+        String nome2;
 
-        Jogo jogo = new Jogo("Nando", "JP");
+        System.out.println("Digite o nome do Jogador 1:");
+        nome1 = scanner.nextLine();
+
+        System.out.println("Digite o nome do Jogador 2:");
+        nome2 = scanner.nextLine();
+
+        Jogo jogo = new Jogo(nome1, nome2);
 
         jogo.iniciar();
+
+        scanner.close();
     }
 }
