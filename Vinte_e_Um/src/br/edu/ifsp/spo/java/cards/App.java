5efb2454b7bcabab572f391e01ef57d726ba36f6
Caíque <135059792+CaFranca@users.noreply.github.com>
2025-04-23 10:36:02 -0300

    Correção na logica de organização da 1.6.2
    existiam arquivos duplicados

---
+++ Vinte_e_Um/src/br/edu/ifsp/spo/java/cards/App.java
@@ -0,0 +1,24 @@
+package br.edu.ifsp.spo.java.cards;
+import br.edu.ifsp.spo.java.cards.partidas.JogoUI;
+import br.edu.ifsp.spo.java.cards.partidas.Jogo;
+public class App {
+    public static void main(String[] args) {
+        JogoUI ui = new JogoUI();
+        String nome1, nome2;
+
+        nome1 = ui.pedirNomeJogador(1);
+        nome2 = ui.pedirNomeJogador(2);
+
+        int modo = ui.escolherModoPontuacao();
+
+        int jogar = 1;
+        while (jogar == 1) {
+            Jogo jogo = new Jogo(nome1, nome2, modo, ui);
+            jogo.iniciar();
+            jogar = ui.perguntarSeAceitaRevanche();
+            ui.exibirMensagem("Jogo encerrado!!");
+        }
+
+        ui.fecharScanner();
+    }
+}
