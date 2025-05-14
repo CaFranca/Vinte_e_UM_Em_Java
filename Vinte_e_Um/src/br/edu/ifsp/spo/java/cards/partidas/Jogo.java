b3054a1110003b5b967c14795394ba9a3307118c
Caíque <135059792+CaFranca@users.noreply.github.com>
2025-05-14 08:41:47 -0300

    Fixing bugs
    A carta dez variava de acrodo com o modo, o que não é logicamnete correto

---
+++ Vinte_e_Um/src/br/edu/ifsp/spo/java/cards/partidas/Jogo.java
@@ -0,0 +1,134 @@
+package br.edu.ifsp.spo.java.cards.partidas;
+import br.edu.ifsp.spo.java.cards.itens.Baralho;
+import br.edu.ifsp.spo.java.cards.itens.Pontuador;
+import br.edu.ifsp.spo.java.cards.itens.Carta;
+
+import java.util.concurrent.TimeUnit;
+
+public class Jogo {
+    private final Jogador jogador1;
+    private final Jogador jogador2;
+    private final Baralho baralho;
+    private final Pontuador pontuador;
+    private final int modo;
+    private final JogoUI ui; // Usando JogoUI
+    private final int tipoJogo;
+
+    private int turno;
+    private boolean jogador1Parou;
+    private boolean jogador2Parou;
+    private boolean jogoFinalizado;
+    public boolean jogoEmpatado;
+
+    public Jogo(String nomeJogador1, String nomeJogador2, int modo, JogoUI ui, int tipoJogo,  Baralho baralho) {
+        if (tipoJogo == 1) { // PvP
+            this.jogador1 = new Jogador(nomeJogador1);
+            this.jogador2 = new Jogador(nomeJogador2);
+        } else if (tipoJogo == 2) { // PvE
+            this.jogador1 = new Jogador(nomeJogador1);
+            this.jogador2 = new IA("IA");
+        } else if (tipoJogo == 3) { // EvE
+            this.jogador1 = new IA("IA 1");
+            this.jogador2 = new IA("IA 2");
+        } else {
+            this.jogador1 = new Jogador(nomeJogador1);
+            this.jogador2 = new Jogador(nomeJogador2);
+        }
+
+        this.baralho = baralho;
+        this.pontuador = new Pontuador();
+        this.modo = modo;
+        this.ui = ui; // Inicializando JogoUI
+        this.turno = 1;
+        this.jogador1Parou = false;
+        this.jogador2Parou = false;
+        this.jogoFinalizado = false;
+        this.jogoEmpatado = false;
+        this.tipoJogo= tipoJogo;
+    }
+
+    public void iniciar() throws InterruptedException {
+        for (int i = 0; i < 2; i++) {
+            jogador1.adicionarCarta(baralho.tirarCarta());
+            jogador2.adicionarCarta(baralho.tirarCarta());
+        }
+
+        while (!jogoFinalizado) {
+            Jogador jogadorAtual = (turno == 1) ? jogador1 : jogador2;
+            boolean jogadorParou = (turno == 1) ? jogador1Parou : jogador2Parou;
+
+            if (!jogadorParou) {
+                ui.exibirMensagem("\n=== Vez de " + jogadorAtual.getNome() + " ===");
+
+                boolean vezEncerrada = false;
+                while (!vezEncerrada) {
+                    int pontuacao = pontuador.verificarPontuacao(jogadorAtual.getCartas(), modo);
+                    ui.exibirPontuacao(jogadorAtual.getNome(), pontuacao);
+
+                    int opcao;
+
+                    if (tipoJogo == 1 || (tipoJogo == 2 && jogadorAtual == jogador1)) {
+                        opcao = ui.perguntarEscolhaJogada();
+                    } else {
+                        IA ia = (IA) jogadorAtual;
+                        opcao = ia.decidirJogada(pontuacao);
+                    }
+
+
+
+                    switch (opcao) {
+                        case 1 -> {
+                            if (baralho.cartasRestantes() == 0) {
+                                ui.exibirMensagem("O baralho acabou! Reembaralhando...");
+                                baralho.resetarBaralho();
+                            }
+                            Carta novaCarta = baralho.tirarCarta();
+
+                            jogadorAtual.adicionarCarta(novaCarta);
+                            ui.exibirCartaPegada(novaCarta);
+                            ui.exibirCartasRestantes(baralho.cartasRestantes());
+                            int novaPontuacao = pontuador.verificarPontuacao(jogadorAtual.getCartas(), modo);
+                            if (novaPontuacao == 21) {
+                                ui.exibirMensagem("Parabéns, você fez 21!");
+                                encerrarJogo(jogadorAtual);
+                                return;
+                            } else if (novaPontuacao > 21) {
+                                ui.exibirMensagem("Estourou! "+jogadorAtual.getNome()+ " fez " + novaPontuacao);
+                                Jogador outro = (jogadorAtual == jogador1) ? jogador2 : jogador1;
+                                encerrarJogo(outro);
+                                return;
+                            }
+                        }
+                        case 2 -> {
+                            ui.exibirMensagem(jogadorAtual.getNome() + " decidiu parar.");
+                            if (turno == 1) jogador1Parou = true;
+                            else jogador2Parou = true;
+                            vezEncerrada = true;
+                        }
+                    }
+                }
+            }
+
+            if (jogador1Parou && jogador2Parou) {
+                int pontos1 = pontuador.verificarPontuacao(jogador1.getCartas(), modo);
+                int pontos2 = pontuador.verificarPontuacao(jogador2.getCartas(), modo);
+
+                if (pontos1 > pontos2) encerrarJogo(jogador1);
+                else if (pontos2 > pontos1) encerrarJogo(jogador2);
+                else {
+                    ui.exibirEmpate(pontos1);
+                    jogoEmpatado = true;
+                }
+                return;
+            }
+
+            turno = (turno == 1) ? 2 : 1;
+        }
+    }
+
+    private void encerrarJogo(Jogador vencedor) {
+        int pontuacao = pontuador.verificarPontuacao(vencedor.getCartas(), modo);
+        ui.exibirVencedor(vencedor.getNome(), pontuacao);
+        jogoFinalizado = true;
+    }
+}
